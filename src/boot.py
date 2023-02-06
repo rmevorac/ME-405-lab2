eccefda2846e852909a4235f0e56d2916b1064ec
LijYem <83598825+LijYem@users.noreply.github.com>
2023-02-05 19:29:47 -0800

    Add files via upload

--- src/boot.py
+++ src/boot.py
@@ -1,9 +1,42 @@
-import serial
+import pyb, utime
+from pyb import Pin as Pin
+from encoder_reader import Encoder
+from motor_driver import MotorDriver
+from controller import Controller
 
-with serial.Serial('COM4',115200) as ser:
-    ser.flush()
+def get_params():
     while True:
         try:
-          print (ser.readline().split(b','))
-        except:
-            print('error')
+            KP = float(input("Enter a KP: "))
+            setpoint = int(input("Enter a setpoint: "))
+            return (KP, setpoint)
+        except ValueError:
+            print("Please enter a valid input")
+            
+    
+if __name__ == "__main__":
+    pyb.repl_uart(None)                    # Turn off the REPL on UART2
+    u2 = pyb.UART(2, baudrate=115200)      # Set up the second USB-serial port
+    
+    motor1 = MotorDriver(Pin.board.PC1, Pin.board.PA0, Pin.board.PA1, 5)
+    encoder1 = Encoder(Pin.board.PB6, Pin.board.PB7, 4)
+    controller1 = Controller(0 , 0, motor1, encoder1)
+    
+    while 1:        
+        try:
+            if controller1.run():
+                u2.write(f"{controller1.motor_data[0]}, {controller1.motor_data[1]}\r\n")
+            
+            if encoder1.position in range(controller1.setpoint - 300, controller1.setpoint + 300):
+                u2.write("end")
+                updated_params = get_params()
+                controller1.time = utime.ticks_ms()
+                controller1.encoder.zero()
+                controller1.set_KP(updated_params[0])
+                controller1.set_setpoint(updated_params[1])
+                
+        except KeyboardInterrupt:
+            motor1.set_duty_cycle(0)
+            print("motor shut off")
+            print(controller1.motor_data)
+            break
--- src/controller.py
+++ src/controller.py
@@ -9,21 +9,24 @@ class Controller:
         self.setpoint = setpoint
         self.motor = motor
         self.encoder = encoder
-        self.motor_data = [(0,0)]
+        self.motor_data = (0,0)
         self.time = utime.ticks_ms()
         print(f"Creating controller with KP {self.KP} and setpoint {self.setpoint}")
 
     def run(self):
-        self.encoder.read()
-        output = self.KP * (self.setpoint - self.encoder.position)
-        self.motor.set_duty_cycle(output)
+        flag = 0
         
         delta_time = utime.ticks_ms() - self.time
-        if delta_time >= 25:
+        if delta_time >= 10:
+            self.encoder.read()
+            output = self.KP * (self.setpoint - self.encoder.position)
+            self.motor.set_duty_cycle(output)
+            
             self.time = utime.ticks_ms()
-            self.motor_data.append((delta_time + self.motor_data[-1][0], self.encoder.position))
+            self.motor_data = (delta_time + self.motor_data[0], self.encoder.position)
+            flag = 1
         
-        print(self.encoder.position)
+        return flag
 
     def set_setpoint(self, setpoint):
         self.setpoint = setpoint
@@ -31,19 +34,4 @@ class Controller:
     def set_KP(self, KP):
         self.KP = KP
 
-
-
-if __name__ == "__main__":
-    motor1 = MotorDriver(Pin.board.PC1, Pin.board.PA0, Pin.board.PA1, 5)
-    encoder1 = Encoder(Pin.board.PB6, Pin.board.PB7, 4)
-    controller1 = Controller(0.0001 , 3200000, motor1, encoder1)
-    
-    while 1:
-        try:
-            controller1.run()
-        except KeyboardInterrupt:
-            motor1.set_duty_cycle(0)
-            print("motor shut off")
-            print(controller1.motor_data)
-            break
             
