b1933bdd9bf22043ea1a94a0277c0ae7a2e7e19f
LijYem <83598825+LijYem@users.noreply.github.com>
2023-02-06 16:31:08 -0800

    Add files via upload

--- src/boot.py
+++ src/boot.py
@@ -7,9 +7,9 @@ from controller import Controller
 def get_params():
     while True:
         try:
-            kp = float(input("Enter a KP: "))
+            KP = float(input("Enter a KP: "))
             setpoint = int(input("Enter a setpoint: "))
-            return (kp, setpoint)
+            return (KP, setpoint)
         except ValueError:
             print("Please enter a valid input")
             
@@ -20,19 +20,22 @@ if __name__ == "__main__":
     
     motor1 = MotorDriver(Pin.board.PC1, Pin.board.PA0, Pin.board.PA1, 5)
     encoder1 = Encoder(Pin.board.PB6, Pin.board.PB7, 4)
-    controller1 = Controller(0 , 0, motor1, encoder1)
+    updated_params = get_params()
+    controller1 = Controller(updated_params[0] , updated_params[1], motor1, encoder1)
     
     while 1:        
         try:
             if controller1.run():
                 u2.write(f"{controller1.motor_data[0]}, {controller1.motor_data[1]}\r\n")
             
-            if encoder1.position in range(controller1.setpoint - 300, controller1.setpoint + 300):
+            if abs(encoder1.position - encoder1.prev_position) <= 2\
+                and encoder1.position in range(controller1.setpoint - 50, controller1.setpoint + 50):
                 u2.write("end")
                 updated_params = get_params()
-                controller1.time = utime.ticks_ms()
                 controller1.encoder.zero()
-                controller1.set_kp(updated_params[0])
+                controller1.time = utime.ticks_ms()
+                controller1.motor_data = (0, 0)
+                controller1.set_KP(updated_params[0])
                 controller1.set_setpoint(updated_params[1])
                 
         except KeyboardInterrupt:
--- src/encoder_reader.py
+++ src/encoder_reader.py
@@ -18,14 +18,12 @@ class Encoder:
         self.ch1 = self.tim.channel(1, pyb.Timer.ENC_AB, pin=self.enc_chA)
         self.ch2 = self.tim.channel(2, pyb.Timer.ENC_AB, pin=self.enc_chB)
         self.position = 0
+        self.prev_position = 1000
         self.old_delta = 0
-#         self.real_time = pyb.millis()
         print ("Creating Encoder")
 
         
     def read(self):
-        #if pyb.elapsed_millis(self.real_time) >= 100:
-#         pyb.delay(10)
         new_delta = self.tim.counter()
         delta_1 = new_delta - self.old_delta
 
@@ -40,29 +38,10 @@ class Encoder:
                 delta_1 = new_delta + 65536 - self.old_delta
                 
         self.old_delta = new_delta
+        self.prev_position = self.position
         self.position -= delta_1
         print(self.position)
-# # #         self.real_time = pyb.millis()
 
-    def working_read(self):
-        #if pyb.elapsed_millis(self.real_time) >= 100:
-#         pyb.delay(10)
-        new_delta = self.tim.counter()
-        delta_1 = new_delta - self.old_delta
 
-        if delta_1 >= 32768:
-            delta_1 -= 65536
-            if new_delta < self.old_delta: #big drop
-                delta_1 = new_delta + 65536 - self.old_delta
-        if delta_1 <= -32768:
-            delta_1 += 65536
-            if new_delta > self.old_delta: #jump
-                delta_1 = new_delta - 65536 - self.old_delta
-                
-        self.old_delta = new_delta
-        self.position += delta_1
-        print(self.position)
-# # #         self.real_time = pyb.millis()
-    
     def zero(self):
         self.position = 0
