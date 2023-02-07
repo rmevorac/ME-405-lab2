5014bb954c025ebd59c1ff7c2bbf273463cfd381
BMUU <elkayamben@gmail.com>
2023-02-06 16:30:43 -0800

    Update motor_decoder.py
    Final functional version

--- src/motor_decoder.py
+++ src/motor_decoder.py
@@ -1,3 +1,18 @@
+"""!@file		encoder_reader.py
+    @brief		This class handles the data coming in from the encoder and parses it for user readability and motor control
+                
+    
+    @author		Ben Elkayam
+    @author		Roey Mevorach
+    @author		Ermias Yemane
+    @date		January 2 6 2023
+
+
+"""
+"""
+@package
+"""
+
 import serial
 import array
 from matplotlib import pyplot as plt
@@ -15,7 +30,7 @@ if __name__ == "__main__":
             
             line = ser.readline()
             print(line)
-            if(line == b''):
+            if(line == b'end'):
                 print('ended')
                 break
             try:
@@ -28,16 +43,18 @@ if __name__ == "__main__":
 
         print('Stop Reading')
 
-    datax = array.array('f', [len(databx)])
-    datay = array.array('f', [len(databy)])
-        
+
+    
+    datax = array.array('f', [0] * len(databx))
+    datay = array.array('f', [0] * len(databy))
+       
     for i in range(len(datax)):
-        datax[i] = float(databx[i])
-        datay[i] = float(databy[i])
+        datax[i] = float(databx[i])/1000
+        datay[i] = float(databy[i])/1000
 
 
     plt.plot(datax,datay)
     plt.axis([min(datax),max(datax),min(datay),max(datay)])
-    plt.xlabel("Time")
-    plt.ylabel("Position")
+    plt.xlabel("Time (S)")
+    plt.ylabel("Position (Rotations)")
     plt.show()
