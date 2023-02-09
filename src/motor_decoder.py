b1195db1924c8451c1089079155877165bd68852
BMUU <elkayamben@gmail.com>
2023-02-08 16:33:17 -0800

    Updated Comments
    thanks homie #4

--- src/motor_decoder.py
+++ src/motor_decoder.py
@@ -1,5 +1,18 @@
-## @file
-# 
+## @file motor_decoder
+# @brief This class handles the data coming in from the encoder and parses it for user readability and motor control
+## @details
+#
+# This script contains code for plotting the position data received from a serial port. The data is collected in real-time and sent to the computer via the serial port.
+#
+# The main function of the script, `if __name__ == "__main__":`, initializes the serial port and receives position data from it. It prompts the user for two input values, the KP and setpoint values, which are sent to the controller via the serial port.
+#
+# The received position data is processed and plotted using the `matplotlib` library. The data is first read from the serial port and stored in two separate lists, `databx` and `databy`. These lists are then converted to arrays of type `float`, and the values are divided by 1000 to get the data in seconds and rotations.
+#
+# Finally, the data is plotted with `matplotlib`, with the time data on the x-axis and the position data on the y-axis. The plot is displayed to the user once all the data has been processed.               
+# @author Ben Elkayam
+# @author Roey Mevorach
+# @author Ermias Yemane
+# @date	January 1 30 2023
 # This file contains code for plotting the position data received from a serial port.
 #
 
@@ -38,22 +51,22 @@ if __name__ == "__main__":
         ## Sending user input to controller
         serSend.write(f"{params[0]}\r\n".encode())
         serSend.write(f"{params[1]}\r\n".encode())
-<<<<<<< HEAD
+#<<<<<<< HEAD
         
         serSend.close()
-=======
->>>>>>> e0903c1f04926ae3f85e405623f2b6211c26c488
+#=======
+#>>>>>>> e0903c1f04926ae3f85e405623f2b6211c26c488
 
 
     ## Opening the serial port 'COM4' with a baud rate of 115200 and a timeout of 7 seconds
-    with serial.Serial('COM4',115200,timeout = 7) as ser:
+    with serial.Serial('COM4',115200,timeout = 1) as ser:
         ## Flushing the input buffer of the serial port
         ser.flush()
-<<<<<<< HEAD
+#<<<<<<< HEAD
         
         
-=======
->>>>>>> e0903c1f04926ae3f85e405623f2b6211c26c488
+#=======
+#>>>>>>> e0903c1f04926ae3f85e405623f2b6211c26c488
 
         ## A loop to continuously read the data from the serial port
         while 1:
@@ -89,7 +102,8 @@ if __name__ == "__main__":
        
     ## Converting the position data from bytes to float and dividing by 1000 to get the data in seconds and rotations
     ##Fix plotting to zero before submission and last value comes in with paraenthesis 
-    for i in range(len(datax) - 1):
+    for i in range(len(datax)):
+        
         datax[i] = float(databx[i])/1000
         datay[i] = float(databy[i])/1000
 
@@ -97,7 +111,7 @@ if __name__ == "__main__":
     ## Plotting the received position data
     plt.plot(datax,datay)
     ## Setting the axis limits of the plot
-    plt.axis([min(datax),max(datax),min(datay),max(datay)])
+    plt.axis([min(datax),max(datax),min(datay),max(datay) + 0.5])
     ## Adding a label to the x-axis of the plot
     plt.xlabel("Time (S)")
     ## Adding a label to the y-axis of the plot
